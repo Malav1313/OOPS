704aaaf0eb2547fae969fc43d69a474ee7deffc6
Malav1313 <145552831+Malav1313@users.noreply.github.com>
2023-10-27 13:29:16 +0000

    indentaiton correction

--- MALAV_PATEL/2303031057065/ageCalculator.java
+++ MALAV_PATEL/2303031057065/ageCalculator.java
@@ -1,20 +1,27 @@
-import java.time.LocalDate; import java.time.Period; import java.util.Scanner;
+import java.time.LocalDate; 
+import java.time.Period; 
+import java.util.Scanner;
 
 public class ageCalculator {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
-LocalDate currentYear = LocalDate.now(); Scanner sc = new Scanner(System.in); System.out.println("Enter the birthYear:"); int birthYear = sc.nextInt();
+        LocalDate currentYear = LocalDate.now(); 
+        Scanner sc = new Scanner(System.in); 
+        System.out.println("Enter the birthYear:"); int birthYear = sc.nextInt();
 
 
-int age = calculateYear(currentYear.getYear(), birthYear); System.out.println("Your age Is " + age);
+        int age = calculateYear(currentYear.getYear(), birthYear);  
+        System.out.println("Your age Is " + age);
 
-}
-public static int calculateYear(int currentYear, int birthYear) { LocalDate currentDate = LocalDate.of(currentYear, 1, 1);
-LocalDate birthDate = LocalDate.of(birthYear, 1, 1);
+    }
+        public static int calculateYear(int currentYear, int birthYear) { 
+            
+            LocalDate currentDate = LocalDate.of(currentYear, 1, 1);
+            LocalDate birthDate = LocalDate.of(birthYear, 1, 1);
 
-Period pe = Period.between(birthDate, currentDate);
-return pe.getYears();
-}
+            Period pe = Period.between(birthDate, currentDate);
+            return pe.getYears();
+    }
 
 }
